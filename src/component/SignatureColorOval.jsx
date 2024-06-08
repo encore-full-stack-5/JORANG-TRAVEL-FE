bcde0816054edd1bd02512627912801e6761d886
Kim Sehyun <kkshyun56@gmail.com>
2024-06-08 16:56:39 +0900

    [feature] mypage

---
+++ src/component/SignatureColorOval.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SignatureColorOval = (props) => {
+  return (
+    <div className="signature-color-oval">
+      <p>{props.content}</p>
+    </div>
+  );
+};
+
+export default SignatureColorOval;
