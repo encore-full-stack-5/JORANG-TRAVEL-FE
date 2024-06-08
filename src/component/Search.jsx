6e25e1ff3ab9c190e5066db7417ba9f60e7bb1fe
Kim Sehyun <kkshyun56@gmail.com>
2024-06-08 12:17:51 +0900

    [feature] login page

--- src/component/Search.jsx
+++ src/component/Search.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import searchImage from "./../image/searchImage.png";
 const Search = () => {
   return (
-    <div className="search">
+    <div className="row-center">
       <input type="text" />
       <img width="25px" height="25px" src={searchImage} alt="Search" />
     </div>
--- src/component/SignIn.jsx
+++ src/component/SignIn.jsx
@@ -1,11 +1,38 @@
 import React from "react";
 import jorangImage from "./../image/jorangImage.png";
+import { Link } from "react-router-dom";
 
 const SignIn = () => {
   return (
     <div>
-      <p>SignIn</p>
-      <img sizes="50px" src={jorangImage}></img>
+      <div className="login">
+        <input
+          type="text"
+          placeholder="ID"
+          style={{ height: "20px", width: "320px", borderRadius: "10px" }}
+        />
+        <input
+          type="password"
+          placeholder="PW"
+          style={{ height: "20px", width: "320px", borderRadius: "10px" }}
+        />
+        <button className="rectangle">
+          <p className="font-color" style={{ color: "#606060" }}>
+            로그인
+          </p>
+        </button>
+      </div>
+
+      <div className="row-center">
+        <Link to="/FindLoginId" style={{ textDecoration: "none" }}>
+          <p className="font-color">아이디</p>
+        </Link>
+        <p className="font-color">/</p>
+        <Link to="/FindPassword" style={{ textDecoration: "none" }}>
+          <p className="font-color">비밀번호</p>
+        </Link>
+        <p className="font-color">찾기</p>
+      </div>
     </div>
   );
 };
