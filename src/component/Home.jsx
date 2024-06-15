93a6858d54529d881128014fd65acef92a2a5efd
jinho9482 <157558300+jinho9482@users.noreply.github.com>
2024-06-15 16:08:01 +0900

    [Feat] Add world map to home page

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,12 +1,39 @@
-import React from "react";
-import jorangImage from "./../image/jorangImage.png";
+import React, { useState } from "react";
+import WorldMap from "react-world-map";
+import "./Home.css";
 
 const Home = () => {
+  const [selected, setSelected] = useState();
+  const [selectedContinent, setSelectedContinent] = useState();
+  const [x, setX] = useState();
+  const [y, setY] = useState();
+  const handleMouseOver = (e) => {
+    if (e.target.id.substring(0, 4) === "path") {
+      // setSelectedContinent(e.target.parentNode.id);
+      console.log(e.pageX);
+      console.log(e.pageY);
+      // showContinent(e);
+      // document.getElementById("continent").value = e.target.parentNode.id;
+    }
+  };
+
+  // const showContinent = (e) => {
+  //   e.preventDefault();
+  //   setX(e.pageX);
+  //   setY(e.pageY);
+  // };
+
+  // const countryInAs = ["한국", "일본", "중국"];
+
   return (
-    <div>
-      <p>Home</p>
-      <img sizes="50px" src={jorangImage}></img>
-    </div>
+    <>
+      <div className="world-map-container" onMouseOver={handleMouseOver}>
+        <WorldMap selected={selected} onSelect={setSelected} />
+      </div>
+      <span id="continent" style={{ top: `${30}%`, left: `${60}%` }}>
+        안녕하세요
+      </span>
+    </>
   );
 };
 
